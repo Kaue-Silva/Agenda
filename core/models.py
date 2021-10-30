00c7c73aae84f95b707767ba8517e885b2206dda
kauesilva <kauesilva940@gmail.com>
2021-10-30 18:50:51 -0300

    Edição e Exclusão de dados

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ class Evento(models.Model):
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
+    local = models.CharField(max_length=30, blank=True, null=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
@@ -16,3 +17,6 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%y %H:%M Hrs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
